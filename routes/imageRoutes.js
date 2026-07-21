69a63f40bee3cb6d5a35747811e7683ddb02f461
Bodoorr <bodoor5@hotmail.com>
2026-07-22 00:33:13 +0300

    debuging by adding logs

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -2,20 +2,6 @@ const router = require("express").Router();
 const upload = require("../middleware/multer");
 const { uploadPhoto } = require("../controllers/imageController.js");
 
-router.post(
-  "/upload",
-  (req, res, next) => {
-    req.on("aborted", () => {
-      console.log("Request aborted by client");
-    });
+router.post("/", upload.single("image"), uploadPhoto);
 
-    req.on("close", () => {
-      console.log("Request closed");
-    });
-
-    next();
-  },
-  upload.single("image"),
-  uploadPhoto,
-);
 module.exports = router;
