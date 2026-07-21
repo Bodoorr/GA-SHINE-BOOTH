acf77dc9424cc4ece041d9fd59dfa84f592a3188
Bodoorr <bodoor5@hotmail.com>
2026-07-21 23:41:45 +0300

    adding logs

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,19 +1,23 @@
-const cloudinary = require('../config/cloudinary')
+const cloudinary = require("../config/cloudinary");
 
 exports.uploadPhoto = (req, res) => {
+  console.log(req.file);
+
   if (!req.file)
-    return res.status(400).json({ success: false, message: 'No file uploaded' })
+    return res
+      .status(400)
+      .json({ success: false, message: "No file uploaded" });
 
   const stream = cloudinary.uploader.upload_stream(
-    { folder: 'ga_shine_booth' },
+    { folder: "ga_shine_booth" },
     (error, result) => {
       if (error) {
-        console.error(error)
-        return res.status(500).json({ success: false, error: error.message })
+        console.error(error);
+        return res.status(500).json({ success: false, error: error.message });
       }
-      res.json({ success: true, url: result.secure_url })
-    }
-  )
+      res.json({ success: true, url: result.secure_url });
+    },
+  );
 
-  stream.end(req.file.buffer)
-}
+  stream.end(req.file.buffer);
+};
